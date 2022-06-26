9309bc7945bf6a002b7e59c3101758580244c79e
katochsenthal <katoch.senthal@gmail.com>
2022-06-26 17:49:10 -0400

    added comments model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
+// One to Many Users have many posts
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
@@ -8,6 +10,26 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
+});
+
+// One to Many Users have many comments
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+// One to Many Posts have many comments
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Post };

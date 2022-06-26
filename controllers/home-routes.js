91198dc28f7d72deb5c1d6bb3e2feca14dd99e4f
katochsenthal <katoch.senthal@gmail.com>
2022-06-26 17:59:12 -0400

    get one post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,46 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { Post, Comment, User } = require("../models");
 
+// home page
 router.get("/", (req, res) => {
   res.render("homepage", { loggedIn: req.session.loggedIn });
 });
 
+// login page
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     return res.redirect("/");
   }
   res.render("login", { loggedIn: req.session.loggedIn });
+});
+
+// Go to specific post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Comment,
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        return res.status(404).json({ message: "No post found with this id" });
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;

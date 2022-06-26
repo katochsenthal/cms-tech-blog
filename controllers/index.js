d55a6ea7ab0a674a9f15c172e1d47c5bd609600f
katochsenthal <katoch.senthal@gmail.com>
2022-06-26 16:11:55 -0400

    homeroute set

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
+const homeRoutes = require("./home-routes");
 
+router.use("/", homeRoutes);
 router.use((req, res) => {
   res.status(404).end();
 });
-
-module.exports = router;

e756197750ce29cf044d03dae41a3384c5d78c69
katochsenthal <katoch.senthal@gmail.com>
2022-06-26 17:14:14 -0400

    logout function implemented

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const { User } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("homepage", { loggedIn: req.session.loggedIn });
 });
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     return res.redirect("/");
   }
-  res.render("login");
+  res.render("login", { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;

90893bbe071af7b055da02e83b5e92a95d01d7be
katochsenthal <katoch.senthal@gmail.com>
2022-06-26 17:09:08 -0400

    unsaved changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,11 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    return res.redirect("/");
+  }
+  res.render("login");
+});
+
 module.exports = router;

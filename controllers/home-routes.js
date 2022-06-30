2448d6f258be1d4a5f8fdcc097a78c8253db7b85
katochsenthal <katoch.senthal@gmail.com>
2022-06-30 18:58:38 -0400

    added home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,29 @@ const { Post, Comment, User } = require("../models");
 
 // home page
 router.get("/", (req, res) => {
-  res.render("homepage", { loggedIn: req.session.loggedIn });
+  Post.findAll({
+    include: [
+      {
+        model: Comment,
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // login page
